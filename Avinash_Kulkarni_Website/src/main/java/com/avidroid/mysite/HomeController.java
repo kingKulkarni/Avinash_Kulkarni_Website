8abefbf9d0fe28f4a8031b84850e66384630bfe9
Avi <Avi@Avi-PC>
2020-06-21 02:44:04 +0530

    HomeConntroller Changes

--- Avinash_Kulkarni_Website/src/main/java/com/avidroid/mysite/HomeController.java
+++ Avinash_Kulkarni_Website/src/main/java/com/avidroid/mysite/HomeController.java
@@ -1,9 +1,6 @@
 package com.avidroid.mysite;
 
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,14 +8,7 @@ public class HomeController {
 
 	@RequestMapping(value = "/imAvi")
 	public String home() {
-		
-		/*Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );*/
-		
+				
 		return "index";
 	}
 }
