a3a4e6268884a8ba358f724e6b6583c05cf9036f
Avi <Avi@Avi-PC>
2020-06-21 02:40:46 +0530

    First Commit

---
+++ Avinash_Kulkarni_Website/src/main/java/com/avidroid/mysite/AvinashKulkarniWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.avidroid.mysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvinashKulkarniWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvinashKulkarniWebsiteApplication.class, args);
+	}
+
+}
---
+++ Avinash_Kulkarni_Website/src/main/java/com/avidroid/mysite/HomeController.java
@@ -0,0 +1,24 @@
+package com.avidroid.mysite;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/imAvi")
+	public String home() {
+		
+		/*Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );*/
+		
+		return "index";
+	}
+}
